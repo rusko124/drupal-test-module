aec50d22f75852372c945b5be902be05b7581158
rusko124 <pavel.lvl@yandex.ru>
2017-04-19 13:53:55 +0600

    Add files via upload

--- ajax_login/src/AjaxLoginSubmit.php
+++ ajax_login/src/AjaxLoginSubmit.php
@@ -14,36 +14,29 @@ use Drupal\Core\Ajax\CssCommand;
 use Drupal\Core\Form\FormStateInterface;
 
 
-class AjaxLoginSubmit
-{
+class AjaxLoginSubmit {
 
-    /**
-     * Ajax form submit callback.
-     *  @param array $form
-     *  @param \Drupal\Core\Form\FormStateInterface $form_state
-     * @return \Drupal\Core\Ajax\AjaxResponse
-     */
+ /*
+  * {@inheritdoc}.
+  */
+  public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
 
-    public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state)
-    {
+    $ajax_response = new AjaxResponse();
 
-        $ajax_response = new AjaxResponse();
+    $auth = \Drupal::currentUser()->isAuthenticated();
 
-        $auth = \Drupal::currentUser()->isAuthenticated();
+    $link_to_title = "<br><a href='/'>DRUPAL LINK </a>";
 
-        $link_to_title = "<br><a href='/'>DRUPAL LINK </a>";
-
-        if ($auth == true) {
-            $ajax_response->addCommand(new HtmlCommand('#' . Html::getClass($form['form_id']['#value']) . '-messages', ' Hello, '.$form_state->getValue('name').'! To see the website as a registered user go to this link.' . $link_to_title));
-            $ajax_response->addCommand(new CssCommand('.form-item-name', ['display' => 'none']));
-            $ajax_response->addCommand(new CssCommand('.form-item-pass', ['display' => 'none']));
-            $ajax_response->addCommand(new CssCommand('#edit-actions', ['display' => 'none']));
-        } else {
-            $ajax_response->addCommand(new HtmlCommand('#' . Html::getClass($form['form_id']['#value']) . '-messages', " Incorrect login and/or password!"));
-
-        };
-
-        return $ajax_response;
-    }
-
+    if ($auth == true) {
+      $ajax_response->addCommand(new HtmlCommand('#' . Html::getClass($form['form_id']['#value']) . '-messages',
+        ' Hello, '.$form_state->getValue('name').'! To see the website as a registered user go to this link.' . $link_to_title)
+      );
+      $ajax_response->addCommand(new CssCommand('.form-item-name', ['display' => 'none']));
+      $ajax_response->addCommand(new CssCommand('.form-item-pass', ['display' => 'none']));
+      $ajax_response->addCommand(new CssCommand('#edit-actions', ['display' => 'none']));
+    } else {
+      $ajax_response->addCommand(new HtmlCommand('#' . Html::getClass($form['form_id']['#value']) . '-messages', " Incorrect login and/or password!"));
+    };
+    return $ajax_response;
+  }
 }
