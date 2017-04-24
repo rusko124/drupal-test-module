c768d11dc4ad540f072242a9584ec2fac76778c3
rusko124 <pavel.lvl@yandex.ru>
2017-04-24 13:00:44 +0600

    Add files via upload

--- status_messager/js/modal_form.js
+++ status_messager/js/modal_form.js
@@ -1,40 +1,41 @@
-  (function ($) {
-      'use strict';
+(function ($) {
+  'use strict';
 
-      Drupal.behaviors.status_messager_behavior = {
-        attach: function (context) {
-          function close(){
-            $('.system-modal-window').remove();
-            $('.system-modal-overlay').remove();
-          }
+  Drupal.behaviors.status_messager_behavior = {
+    attach: function (context,settings) {
+      function close(){
+        $(context).find('.system-modal-window').remove();
+        $(context).find('.system-modal-overlay').remove();
+      }
 
-          if(!($('.system-modal-window.invisible').length)){
-            $('.system-modal-overlay').click(close);
-            $('.close-button').click(close);
+      if(!($(context).find('.system-modal-window.invisible').length)){
+        $(context).find('.system-modal-overlay').click(close);
+        $(context).find('.close-button').click(close);
 
             // // draggable
-            $('body').on('mousedown', '.modal-title', function() {
-              $('.system-modal-window').addClass('draggable').parents().on('mousemove', function(e) {
-                $('.draggable').offset({
-                  top: e.pageY - 15,
-                  left: e.pageX - $('.modal-title').outerWidth() / 2
-                }).on('mouseup', function() {
-                    $('.system-modal-window').removeClass('draggable');
-                });
-              });
+        $(context).find('body').on('mousedown', '.system-modal-window-title', function() {
+          $(context).find('.system-modal-window').addClass('draggable').parents().on('mousemove', function(e) {
+            $(context).find('.draggable').css({'-webkit-user-select':'none'})
+            $(context).find('.draggable').offset({
+              top: e.pageY - 15,
+              left: e.pageX - $('.system-modal-window-title').outerWidth() / 2
             }).on('mouseup', function() {
-              $('.draggable').removeClass('draggable');
+              $(context).find('.system-modal-window').removeClass('draggable');
             });
+          });
+        }).on('mouseup', function() {
+          $(context).find('.draggable').css({'-webkit-user-select':'auto'})
+          $(context).find('.draggable').removeClass('draggable');
+        });
 
             // settings
-            $('.system-modal-window').css({'width': drupalSettings.status_messager.width,
-              'height': drupalSettings.status_messager.height,
-              'left':'calc((100% - '+drupalSettings.status_messager.width+'px)/2)',
-              'top':'calc((100% - '+drupalSettings.status_messager.height+'px)/2)'
-            });
-            $(".system-modal-window-body").css({'background-color': drupalSettings.status_messager.color});
-
-          }
-        }
+        $(context).find('.system-modal-window').css({'width': settings.status_messager.width,
+          'height': settings.status_messager.height,
+          'left':'calc((100% - '+settings.status_messager.width+'px)/2)',
+          'top':'calc((100% - '+settings.status_messager.height+'px)/2)'
+        });
+        $(context).find('#status-messager').css({'background-color': settings.status_messager.color});
       }
-  }(jQuery));
+    }
+  }
+}(jQuery));
