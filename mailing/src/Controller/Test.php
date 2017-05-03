dce49e9dab984b19a8eaf76824f9cfb20a10067a
rusko124 <pavel.lvl@yandex.ru>
2017-05-03 15:36:39 +0600

    Add files via upload

--- mailing/src/Controller/Test.php
+++ mailing/src/Controller/Test.php
@@ -6,14 +6,6 @@ use Drupal\Core\Controller\ControllerBase;
 class Test extends ControllerBase {
   public function test() {
     $output = array();
-	// $queue_mail = \Drupal::queue('mail');
-	// $item = $queue_mail->claimItem();
-	// print_r($item->data['username']);
-	// print_r($item->data['mail']);
-
-	$mail = ['1' => 'hi', '2' => 'bye'];
-	print_r($mail);
-
     return $output;
   }
 }
